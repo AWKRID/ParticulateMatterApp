69ccbe5358b060b82ccd28274010a8cc0a0ab9a6
AWKRID <minzoo202738@gmail.com>
2024-06-21 15:03:37 +0900

    :tada: create: Initialize new project

---
+++ src/main/kotlin/com/awkrid/particulatematter/ParticulateMatterApplication.kt
@@ -0,0 +1,11 @@
+package com.awkrid.particulatematter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParticulateMatterApplication
+
+fun main(args: Array<String>) {
+    runApplication<ParticulateMatterApplication>(*args)
+}
---
+++ src/test/kotlin/com/awkrid/particulatematter/ParticulateMatterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.awkrid.particulatematter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParticulateMatterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
